dcb032f7ac30498ea65a49f2bc65c70f1166f9be
Amir Hamza <hamza_amir@yahoo.com>
2019-06-03 04:56:23 +0600

    Updates

---
+++ precache-manifest.ff2740a3dd2b3a230915b0529726ef0b.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bddb2cb428a0d1d660c23f8ebbdce59",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "3a43d9c97395cce08a7a",
+    "url": "/seasons/static/css/main.453e2d53.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "3a43d9c97395cce08a7a",
+    "url": "/seasons/static/js/main.00aa7cc6.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  },
+  {
+    "url": "/seasons/static/media/9b2fb793e108c5bbc216a1b501067a1a.9b2fb793.jpg"
+  },
+  {
+    "revision": "8e71c1472b5f83b747fb6b66aada475b",
+    "url": "/seasons/static/media/screen-9.8e71c147.jpg"
+  }
+]);
